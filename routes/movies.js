3dda5b8184e207d9d894fe05de31883ca6c25eae
maddockj1 <jmmaddock@gmail.com>
2018-10-19 09:54:22 -0600

    Routes Rooted

--- routes/movies.js
+++ routes/movies.js
@@ -48,20 +48,54 @@ router.post('/', (req, res, next) => {
     })
 })
 // UPDATE ONE record for this table
-router.put('/:id', (req, res, next) => {
+router.patch('/:id', (req, res, next) => {
 
   knex('movies')
   .where('id', req.params.id)
   .then ((results) => {
     if(results.length>0) {
       let myRecord = results[0]
-      if(req.body.title) { myRecord.title = req.body.title}
+      if(req.body.title) { myRecord.title = req.body.title }
+      if(req.body.director) { myRecord.director = req.body.director }
+      if(req.body.year) { myRecord.year= req.body.year }
+      if(req.body.rating) { myRecord.rating = req.body.rating }
+      if(req.body.poster) { myRecord.poster = req.body.poster }
+      knex('movies')
+      .update(myRecord)
+      .where('id', req.params.id)
+      .returning('*')
+      .then((updatedRecord) => {
+        res.send(updatedRecord)
+      })
+
+    }else {
+      throw new Error('Ya Dingus. NOT FoUnD.')
     }
   })
-res.send('UPDATED RECORD')
+  .catch((err) => {
+    next(err)
+  })
 })
 // DELETE ONE record for this table
 router.delete('/:id', (req, res, next) => {
-res.send('DELETED RECORD')
+  knex('movies')
+  .where('id', req.params.id)
+  .then((foundRecords) => {
+    if( foundRecords.length>0 ) {
+      knex('movies').del()
+      .where('id', req.params.id)
+      .returning('*')
+      .then((results) => {
+        let deletedRecord = results[0]
+        res.send(deletedRecord)
+      })
+    } else {
+      throw new Error(`Cant delete what does not exist`)
+    }
+  })
+  .catch((err) => {
+    next(err)
+  })
 })
+
 module.exports = router
