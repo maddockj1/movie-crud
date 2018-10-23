6f0232027a91bebbfd5a11c1b6b73cea393cac34
maddockj1 <jmmaddock@gmail.com>
2018-10-23 15:54:50 -0600

    who knows really whats going on with this insane code anymore

--- public/main.js
+++ public/main.js
@@ -8,104 +8,107 @@ document.addEventListener("DOMContentLoaded", function(event) {
 function getReports() {
   axios.get('/movies')
     .then((response) => {
-        // handle success
-        console.log(response.data);
-        //clear out reports
-        let tbody = document.querySelector('#movies')
-        while (tbody.firstChild) {
-          tbody.removeChild(tbody.firstChild)
-        }
+      // handle success
+      console.log(response.data);
+      //clear out reports
+      let tbody = document.querySelector('#movies')
+      while (tbody.firstChild) {
+        tbody.removeChild(tbody.firstChild)
+      }
 
-        //DOM so table elements up in here
+      //DOM so table elements up in here
 
-        response.data.forEach((report) => {
-          let tr = document.createElement('tr')
-          let title = document.createElement('td')
-          let director = document.createElement('td')
-          let year = document.createElement('td')
-          let myRating = document.createElement('td')
-          let poster = document.createElement('td')
-          let del_td = document.createElement('td')
-          let deleteButton = document.createElement('button')
-          let posterSrc = document.createElement('img')
-          let up_td = document.createElement('td')
-          let updateButton = document.createElement('button')
-          title.innerText = report.Title
-          director.innerText = report.Director
-          year.innerText = report.Year
-          myRating.innerText = report.myRating
-          posterSrc.src = report.poster
-          deleteButton.innerText = "X"
-          deleteButton.setAttribute('data-id', report.id)
-          deleteButton.addEventListener('click', (ev) => {
-            let recordId = ev.target.getAttribute('data-id')
-            //console.log('id', recordId);
-            //delete axios stlyzs
-            axios.delete(`/movies/${recordId}`)
-              .then((response) => {
-                console.log(response)
-                ev.target.parentElement.parentElement.remove()
-              })
-              .catch((err) => {
-                console.log(err)
-              })
-          })
-          updateButton.innerText = "Update"
-          updateButton.setAttribute('up-id', report.id)
-          let titleF = document.getElementById('title')
-          //console.log(titleF.innerText)
-          let directorF = document.getElementById('director')
-          let yearF = document.getElementById('year')
-          let myRatingF = document.getElementById('myRating')
-          let posterF = document.getElementById('poster')
-          updateButton.addEventListener('click', (ev) => {
-            let recordId = ev.target.getAttribute('up-id')
-            //console.log('id', recordId);
-              ev.target.parentElement.remove()
-              let upData = response.data[0]
-              console.log(upData.Title)
-              titleF.value= upData.Title
-              directorF.value =upData.Director
-              yearF.value = upData.Year
-              myRatingF.value = upData.myRating
-              posterF.value = upData.poster
-              })
-    document.getElementById('update-button').addEventListener('click', (ev) => {
+      response.data.forEach((report) => {
+        let tr = document.createElement('tr')
+        let title = document.createElement('td')
+        let director = document.createElement('td')
+        let year = document.createElement('td')
+        let myRating = document.createElement('td')
+        let poster = document.createElement('td')
+        let del_td = document.createElement('td')
+        let deleteButton = document.createElement('button')
+        let posterSrc = document.createElement('img')
+        let up_td = document.createElement('td')
+        let updateButton = document.createElement('button')
+        title.innerText = report.title
+        director.innerText = report.director
+        year.innerText = report.year
+        myRating.innerText = report.myRating
+        posterSrc.src = report.poster
+        deleteButton.innerText = "X"
+        deleteButton.setAttribute('data-id', report.id)
+        deleteButton.addEventListener('click', (ev) => {
+          let recordId = ev.target.getAttribute('data-id')
+          //console.log('id', recordId);
+          //delete axios stlyzs
+          axios.delete(`/movies/${recordId}`)
+            .then((response) => {
+              console.log(response)
+              ev.target.parentElement.parentElement.remove()
+            })
+            .catch((err) => {
+              console.log(err)
+            })
+        })
+        updateButton.innerText = "Update"
+        updateButton.setAttribute('up-id', report.id)
+        let titleF = document.getElementById('title')
+        //console.log(titleF.innerText)
+        let directorF = document.getElementById('director')
+        let yearF = document.getElementById('year')
+        let myRatingF = document.getElementById('myRating')
+        let posterF = document.getElementById('poster')
+        updateButton.addEventListener('click', (ev) => {
+          axios.get(`/movies/${recordId}`)
+          .then((response) => {
+          var recordId = ev.target.getAttribute('up-id')
+          //console.log('id', recordId);
+          // ev.target.parentElement.remove()
+          console.log('up-id', ev.target)
+          let upData = response.data[0]
+          console.log(upData.title)
+          titleF.value = upData.title
+          directorF.value = upData.director
+          yearF.value = upData.year
+          myRatingF.value = upData.myRating
+          posterF.value = upData.poster
+        })
+        document.getElementById('update-button').addEventListener('click', (ev) => {
           ev.preventDefault()
 
-      axios.patch(`/movies/${recordId}`, )
-        .then((response) => {
-          //console.log(response.data[0].Title)
-          
+          axios.patch(`/movies/${recordId}`)
+            .then((response) => {
+              //console.log(response.data[0].Title)
 
-          //ev.target.parentElement.
+
+              //ev.target.parentElement.
+            })
+            .catch((err) => {
+              console.log(err)
+            })
         })
-        .catch((err) => {
-          console.log(err)
-        })
+
+
+
+        up_td.appendChild(updateButton)
+        del_td.appendChild(deleteButton)
+        tr.appendChild(title)
+        tr.appendChild(director)
+        tr.appendChild(year)
+        tr.appendChild(myRating)
+        tr.appendChild(poster)
+        poster.appendChild(posterSrc)
+        tr.appendChild(del_td)
+        tr.appendChild(up_td)
+        tbody.appendChild(tr)
+
+      })
+
     })
-
-
-
-          up_td.appendChild(updateButton)
-          del_td.appendChild(deleteButton)
-          tr.appendChild(title)
-          tr.appendChild(director)
-          tr.appendChild(year)
-          tr.appendChild(myRating)
-          tr.appendChild(poster)
-          poster.appendChild(posterSrc)
-          tr.appendChild(del_td)
-          tr.appendChild(up_td)
-          tbody.appendChild(tr)
-
-        })
-
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-    }
+    .catch((err) => {
+      console.log(err)
+    })
+})
 
 
 
@@ -115,8 +118,8 @@ function getReports() {
 
 
 
-function addNew(){
-  let form= document.getElementById('add-movie')
+function addNew() {
+  let form = document.getElementById('add-movie')
 
   form.addEventListener('submit', (ev) => {
     ev.preventDefault()
@@ -126,9 +129,9 @@ function addNew(){
     let postData = {}
     let formElements = ev.target.elements
 
-    for(var i=0; i< formElements.length; i++) {
+    for (var i = 0; i < formElements.length; i++) {
       let inputName = formElements[i].name
-      if( inputName ) {
+      if (inputName) {
         postData[inputName] = formElements[i].value
       }
     }
@@ -136,12 +139,12 @@ function addNew(){
 
     //post them datas from the front end to the back whhhhhaaaatt???
     axios.post('/movies', postData)
-    .then((response) => {
-      console.log(response)
-      getReports()
-    })
-    .catch((err) => {
-      console.log(err)
-    })
+      .then((response) => {
+        console.log(response)
+        getReports()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   })
 }
